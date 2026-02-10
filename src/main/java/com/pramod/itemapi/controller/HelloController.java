dd58b50468cb4da3788781b8409dc6f8406a32c7
itspramodrana1 <ranapramod663@gmail.com>
2026-02-10 15:49:04 +0530

    Initial commit : Hello API

---
+++ src/main/java/com/pramod/itemapi/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.pramod.itemapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Item API is running";
+    }
+}
